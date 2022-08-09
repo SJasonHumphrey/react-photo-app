a453fb4cd33ad7e81332caf00f9d237dadf324a0
SJasonHumphrey <sjasonhumphrey@gmail.com>
2022-08-09 10:51:58 -0400

    changes to header and added search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { FaPhotoVideo } from "react-icons/fa";
 
 const Header = () => {
   return (
     <div className="pageHeader">
-        <h3>ByThePixel</h3>
+      <div className="icon">
+        <FaPhotoVideo />
+      </div>
+      <h3>ByThePixel</h3>
+      <input className="search" type="text" placeholder="Search" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
