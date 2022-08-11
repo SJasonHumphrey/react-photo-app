e913a0acee511e358304082eeb1e7b0df9fa4874
SJasonHumphrey <sjasonhumphrey@gmail.com>
2022-08-10 22:35:10 -0400

    added search feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPhotoVideo } from "react-icons/fa";
+import Unsplash, { toJson } from "unsplash-js";
+
+const unsplash = new Unsplash({
+  accessKey: "",
+});
 
 const Header = () => {
+
+  const [search, setSearch] = useState("");
+  const [pics, setPics] = useState([]);
+
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+
+    unsplash.search
+      .photos(search)
+      .then(toJson)
+      .then((json) => {
+
+        setPics(json.results);
+      });
+
+  };
+
   return (
     <div className="pageHeader">
       <div className="icon">
         <FaPhotoVideo />
       </div>
       <h3>ByThePixel</h3>
-      <input className="search" type="text" placeholder="Search" />
+      <input
+          type="text"
+          name="query"
+          className="search"
+          placeholder="Search Photos"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
     </div>
   );
 };
